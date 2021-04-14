285e12f27ca244d8b60a816666128103b5736550
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2021-04-14 17:20:50 +0200

    Add retry logic

--- create_project.sh
+++ create_project.sh
@@ -20,15 +20,16 @@ END
 shipyard_encoded="$(echo "$shipyard" | base64 | tr -d \\n )"
 
 echo "Creating project $KEPTN_PROJECT"
-for i in {1..3}
+for i in 1 2 3
 do
+  echo "Attempt $i ..."
   status="$(curl -X POST "${KEPTN_SHIPYARD_CONTROLLER}/v1/project" -H "accept: application/json" -H "Content-Type: application/json"  --write-out %{http_code} -d "{ \"name\": \"${KEPTN_PROJECT}\", \"shipyard\": \"${shipyard_encoded}\"}")"
   if [[ "$status" == 201 ]]; then
     echo "Finished setting up the sample environment"
     exit 0
   fi
   echo "Failed to create Keptn project $KEPTN_PROJECT. HTTP Status code $status"
-  sleep "$((5 * i))"
+  sleep "$((5 * $i))"
 done
 
 exit 1
